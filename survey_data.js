614bf76612af69ea4c9180495475eae4d06d2235
Thomas Pun <77042+tpun@users.noreply.github.com>
2020-09-17 15:45:26 +0800

    Update survey_data.js
    added three mock interview questions

--- survey_data.js
+++ survey_data.js
@@ -1,15 +1,35 @@
-// Hard coded survey questions
+// // Hard coded survey questions
+// module.exports = [
+//     {
+//         text: 'Please tell us your age.',
+//         type: 'number'
+//     },
+//     {
+//         text: 'Have you ever jump-kicked a lemur?',
+//         type: 'boolean'
+//     },
+//     {
+//         text: 'Who is your favorite Teenage Mutant Ninja Turtle and why?',
+//         type: 'text'
+//     }
+// ];  
+
 module.exports = [
     {
-        text: 'Please tell us your age.',
-        type: 'number'
-    },
-    {
-        text: 'Have you ever jump-kicked a lemur?',
-        type: 'boolean'
-    },
-    {
-        text: 'Who is your favorite Teenage Mutant Ninja Turtle and why?',
+        text: 'Could you do a quick 3-minute introduction of yourself?',
         type: 'text'
-    }
-];
+    },
+    {
+        text: 'Throughout your careers, what achievement are you most proud of?',
+        type: 'text'
+    },
+    {
+        text: 'Could you tell me something you are passionate about and how did you discover that?',
+        type: 'text'
+    },
+    {
+        text: 'Which job, company or team did you enjoy working with the most and what made it special?',
+        type: 'text'
+    },
+    
+];  
